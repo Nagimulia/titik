c549a0a0525a8588ff20861b2fba78d12db57178
Nagimulia <keneuova@mail.ru>
2022-11-03 05:49:14 +0600

    create app

---
+++ components/NoResults.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MdOutlineVideocamOff } from 'react-icons/md';
+import { BiCommentX } from 'react-icons/bi';
+
+interface Iprops {
+  text: string;
+}
+const NoResults = ({ text }: Iprops) => {
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {text === 'No comments yet! Be the first one to add a comment' ? (
+          <BiCommentX />
+        ) : (
+          <MdOutlineVideocamOff />
+        )}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
+};
+
+export default NoResults;
